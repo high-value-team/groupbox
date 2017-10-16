f727b2673f89cea08322d8625bc823264873a8c6
Florian Boehmak <florian@fnbk.cc>
2017-10-16 20:01:59 +0200

    send emails with google mail server

---
+++ playground/send_email.go
@@ -0,0 +1,123 @@
+
+package main
+
+import (
+	"os"
+	"strings"
+	"net/smtp"
+	"fmt"
+	"bytes"
+	"mime/quotedprintable"
+)
+
+// usage:
+// export USERNAME="groupbox@googlemail.com"
+// export PASSWORD="geheim"
+// go run send_email.go
+func main() {
+
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+
+	sender := NewSender(username, password)
+
+	Receiver := []string{"abc@gmail.com"}
+
+	Subject := "Groupbox Einladung"
+	message := `
+	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
+	</head>
+	Herzlich Willkommen!<br>
+	<br>
+	Eine neue Groupbox wurde eingerichtet:	<a href="https://www.gropubox.io/mein-box-key">https://www.gropubox.io/mein-box-key</a><br>
+	<br>
+	Viel Spass!<br>
+	</body>
+	</html>
+	`
+	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
+
+	sender.SendMail(Receiver, Subject, bodyMessage)
+}
+
+// The sources below are taken from sources taken from: https://github.com/tangingw/go_smtp
+const (
+	SMTPServer = "smtp.gmail.com" // Gmail SMTP Server
+)
+
+type Sender struct {
+	User     string
+	Password string
+}
+
+func NewSender(Username, Password string) Sender {
+
+	return Sender{Username, Password}
+}
+
+func (sender Sender) SendMail(Dest []string, Subject, bodyMessage string) {
+
+	msg := "From: " + sender.User + "\n" +
+		"To: " + strings.Join(Dest, ",") + "\n" +
+		"Subject: " + Subject + "\n" + bodyMessage
+
+	err := smtp.SendMail(SMTPServer+":587",
+		smtp.PlainAuth("", sender.User, sender.Password, SMTPServer),
+		sender.User, Dest, []byte(msg))
+
+	if err != nil {
+
+		fmt.Printf("smtp error: %s", err)
+		return
+	}
+
+	fmt.Println("Mail sent successfully!")
+}
+
+func (sender Sender) WriteEmail(dest []string, contentType, subject, bodyMessage string) string {
+
+	header := make(map[string]string)
+	header["From"] = sender.User
+
+	receipient := ""
+
+	for _, user := range dest {
+		receipient = receipient + user
+	}
+
+	header["To"] = receipient
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", contentType)
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	message := ""
+
+	for key, value := range header {
+		message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	var encodedMessage bytes.Buffer
+
+	finalMessage := quotedprintable.NewWriter(&encodedMessage)
+	finalMessage.Write([]byte(bodyMessage))
+	finalMessage.Close()
+
+	message += "\r\n" + encodedMessage.String()
+
+	return message
+}
+
+func (sender *Sender) WriteHTMLEmail(dest []string, subject, bodyMessage string) string {
+
+	return sender.WriteEmail(dest, "text/html", subject, bodyMessage)
+}
+
+func (sender *Sender) WritePlainEmail(dest []string, subject, bodyMessage string) string {
+
+	return sender.WriteEmail(dest, "text/plain", subject, bodyMessage)
+}
