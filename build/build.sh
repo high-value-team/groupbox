cb614e309bf944e7451cee4054dba22ad5a59530
Ralf Westphal <info@ralfw.de>
2017-10-17 13:00:00 +0200

    -

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 mkdir ../dropstack
 
-# GROUPBOX_STACKLETNAME in ~/.bashrc definiert (ralfw)
-#sed -i "s/$stackletname/$GROUPBOX_STACKLETNAME" template.dropstack.json
+# GROUPBOX_STACKLETNAME in ~/.bashrc definiert (ralfw), für den Namen
 sed s!'$stackletname'!$GROUPBOX_STACKLETNAME! < template.dropstack.json > ../dropstack/.dropstack.json
 
 cp dropstackdemo.html ../dropstack/index.html
+
+cd ../dropstack
+
+echo "Jetzt das Deployment starten mit: dropstack deploy"
