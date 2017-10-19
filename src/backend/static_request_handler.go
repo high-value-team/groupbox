c5d046467966f80519d3812cdbb292dac97220b0
Florian Boehmak <florian@fnbk.cc>
2017-10-19 14:46:19 +0200

    fix import statement

--- src/backend/static_request_handler.go
+++ src/backend/static_request_handler.go
@@ -2,10 +2,11 @@ package backend
 
 import (
 	"net/http"
-	"github.com/ralfw/groupbox/src/frontend"
+
+	"github.com/high-value-team/groupbox/src/frontend"
 )
 
-type StaticRequestHandler struct {}
+type StaticRequestHandler struct{}
 
 func (handler *StaticRequestHandler) TryHandle(writer http.ResponseWriter, reader *http.Request) bool {
 	http.FileServer(frontend.FS(false)).ServeHTTP(writer, reader)
