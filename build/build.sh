8a36a3cb8f66d87ff6668278deaff4b8cdc1d9f4
Karl Ostendorf <karl@ostendorf.com>
2017-10-19 14:02:58 +0200

    set version number

--- build/build.sh
+++ build/build.sh
@@ -10,11 +10,11 @@ sed s!'$stackletname'!$GROUPBOX_STACKLETNAME! < template.dropstack.json > ../dro
 echo "go build"
 export GOOS=linux
 export GOARCH=amd64
-export VERSION_NUMBER=0.0.3
+export VERSION_NUMBER=`git describe --always --tags --dirty="*"`
 go build -ldflags "-X main.VersionNumber=$VERSION_NUMBER" -o ../dropstack/groupbox ../src
 
 # docker image
 echo "create Dockerfile"
 cp Dockerfile ../dropstack/Dockerfile
-
+ 
 echo "Jetzt nach ../dropstack wechseln und deployen mit dropstack deploy"
