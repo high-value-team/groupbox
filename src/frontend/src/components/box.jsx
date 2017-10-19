0299816f796630bb68ebaaa3a5e0cdabc5653d6f
Karl Ostendorf <karl@ostendorf.com>
2017-10-19 18:30:39 +0200

    add box page

---
+++ src/frontend/src/components/box.jsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import { withStyles } from 'material-ui/styles';
+import { LinearProgress } from 'material-ui/Progress';
+
+import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import FavoriteIcon from 'material-ui-icons/Favorite';
+import Tooltip from 'material-ui/Tooltip';
+import red from 'material-ui/colors/red';
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  card: {
+    float: 'left',
+    margin: '0.5em',
+    maxWidth: 400,
+  },
+  media: {
+    height: 194,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  flexGrow: {
+    flex: '1 1 auto',
+  },
+});
+
+class Box extends React.Component {
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    boxkey: PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 0, // 0: none, 1: loading, 2: loaded - ok, 3: loaded - error
+      box: null,
+    };
+  }
+
+  componentDidMount() {
+    this.loadBox();
+  }
+
+  loadBox() {
+    this.setState({ status: 1 });
+    fetch(`/api/boxes/${this.props.boxkey}`).then(rsp => {
+      if (rsp.ok) {
+        rsp.json().then(box => {
+          const title = `${box.title} - Groupbox`;
+          window.history.replaceState({}, title, '/');
+          document.title = title;
+          this.setState({ box, status: 2 });
+        });
+      } else {
+        this.setState({ status: 3 });
+      }
+    });
+
+  }
+
+  renderBlank() {
+    return <div className={this.props.classes.root} />;
+  }
+
+  renderLoading() {
+    return (
+      <div className={this.props.classes.root}>
+        <LinearProgress />
+      </div>
+    );
+  }
+
+  renderLoadedError() {
+    return (
+      <div className={this.props.classes.root}>
+        <p>Die Box kann leider nicht gefunden werden.</p>
+      </div>
+    );
+  }
+
+  renderItem(index, item) {
+    const { classes } = this.props;
+
+    const authorInitials = item.authorNickname
+      .split(/\s+/)
+      .map(word => word.substr(0, 1))
+      .reduce((initials, letter) => `${initials}${letter}`, '');
+
+    const title = item.message
+      .split(/\n+/)[0].substr(0, 20);
+
+    return (
+      <Card className={classes.card} key={`item-${index}`} >
+        <CardHeader
+          avatar={
+            <Tooltip id="avatar-tooltip" title={item.authorNickname} placement="right">
+              <Avatar aria-label="Author" className={classes.avatar}>
+                {authorInitials}
+              </Avatar>
+            </Tooltip>
+          }
+          title={title}
+          subheader={moment(item.creationDate).fromNow()}
+        />
+        <CardContent>
+          <Typography component="p">
+            {item.message}
+          </Typography>
+        </CardContent>
+        <CardActions disableActionSpacing={true}>
+          <Tooltip id="favorite-tooltip" title="Finde ich gut!" placement="right">
+            <IconButton aria-label="Finde ich gut!">
+              <FavoriteIcon />
+            </IconButton>
+          </Tooltip>
+          <div className={classes.flexGrow} />
+        </CardActions>
+      </Card>
+    );
+  }
+
+  renderLoadedOK() {
+
+    const { classes } = this.props;
+    const { box } = this.state;
+
+    const items = box.items.sort((a, b) => b.creationDate > a.creationDate );
+
+    return (
+      <div className={classes.root}>
+        <h1>{box.title}</h1>
+        <div>
+          {items.map((item, index) => this.renderItem(index, item))}
+        </div>
+      </div>
+    );
+
+  }
+
+  render() {
+
+    switch (this.state.status) {
+      case 1:
+        return this.renderLoading();
+      case 2:
+        return this.renderLoadedOK();
+      case 3:
+        return this.renderLoadedError();
+    }
+
+    return this.renderBlank();
+
+  }
+
+}
+
+export default withStyles(styles)(Box);
--- src/frontend/src/components/home.jsx
+++ src/frontend/src/components/home.jsx
@@ -1,4 +1,102 @@
 import React from 'react';
-export default () => {
-  return <div />;
-};
+import PropTypes from 'prop-types';
+
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+    fontFamily: 'Roboto, sans-serif',
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+});
+
+class Home extends React.Component {
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      ownerEmail: '',
+    };
+  }
+
+  doSubmit = () => {
+    console.log('doSubmit', this.state.name, this.state.ownerEmail);
+  };
+
+  updateName = e => this.setState({ name: e.target.value });
+  updateOwnerEmail = e => this.setState({ ownerEmail: e.target.value });
+
+  render() {
+
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid container={true} spacing={24} justify="center">
+
+          <Grid item={true} xs={12}>
+            <Paper className={classes.paper} elevation={0}>
+              Groupbox anlegen
+            </Paper>
+          </Grid>
+
+          <Grid item={true} xs={12}>
+            <TextField
+              id="name"
+              label="Etikett"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.updateName}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item={true} xs={12}>
+            <TextField
+              id="ownerEmail"
+              label="Wer bist Du?"
+              placeholder="Deine Mailadresse"
+              className={classes.textField}
+              value={this.state.ownerEmail}
+              onChange={this.updateOwnerEmail}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item={true} xs={12}>
+            <Button raised={true} color="primary" className={classes.button} onClick={this.doSubmit}>
+              Los!
+            </Button>
+          </Grid>
+
+        </Grid>
+      </div>
+    );
+
+  }
+
+}
+
+
+export default withStyles(styles)(Home);
