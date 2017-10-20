b55cd95af205351a7b9e1706a69907b8f889166f
Karl Ostendorf <karl@ostendorf.com>
2017-10-20 10:22:55 +0200

    clean up VersionService rxjs, add retry

--- src/frontend/src/services/version.js
+++ src/frontend/src/services/version.js
@@ -1,17 +1,33 @@
-import { Subject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-const VersionService = new Subject().publishReplay(1).refCount();
+const interval = 60000;
+const retryInterval = interval;
 
-export function getVersion() {
-  fetch('/api/version').then(rsp => {
-    if (rsp.ok) {
-      rsp.json().then(version => VersionService.next(version));
-    } else {
-      console.log('Error retrieving version.');
+const version = Observable.create(observer => {
+
+  const getVersion = () => {
+    try {
+      fetch('/api/version').then(rsp => {
+        if (rsp.ok) {
+          rsp.json().then(version => observer.next(version));
+        } else {
+          observer.error(new Error(`${rsp.status} - ${rsp.statusText}`));
+        }
+      });
+    } catch (err) {
+      observer.error(err);
     }
-  });
-}
+  };
 
-getVersion();
+  getVersion(); // seed value
+  const intervalID = setInterval(getVersion, interval);
 
-export default VersionService;
+  return () => {
+    clearInterval(intervalID);
+  };
+
+});
+
+export default version
+  .retryWhen(errors => errors.delayWhen(() => Observable.timer(retryInterval)))
+  .multicast(new BehaviorSubject({})).refCount();
