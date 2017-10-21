45b35977653ec4da909a92901f59d1553f908891
Ralf Westphal <info@ralfw.de>
2017-10-21 09:49:30 +0200

    Item ctor + subject extraction verbessert

--- src/backend/interactions_test.go
+++ src/backend/interactions_test.go
@@ -51,26 +51,3 @@ func TestMapToBoxDTO(t *testing.T) {
 		t.Errorf("mapToBoxDTO() failed!\nexpected:\n%+v\nactual:\n%+v\n", expected, actual)
 	}
 }
-
-
-func TestExtractSubject(t *testing.T) {
-	result := extractSubject("12345678901234567890")
-	if result != "123456789012345..." {
-		t.Errorf("Expected subject shorter than message; result: <%s>", result)
-	}
-
-	result = extractSubject("")
-	if result != "?" {
-		t.Errorf("Expected subject <?> for empty message", result)
-	}
-
-	result = extractSubject("1234567890")
-	if result != "1234567890" {
-		t.Errorf("Expected subject equal to message shorter than max subject len; result <%s>", result)
-	}
-
-	result = extractSubject("123456\n7890")
-	if result != "123456 7890" {
-		t.Errorf("Expected subject to contain no new line.", result)
-	}
-}
--- src/backend/model.go
+++ src/backend/model.go
@@ -1,5 +1,10 @@
 package backend
 
+import (
+	"strings"
+	"time"
+)
+
 type Box struct {
 	Title        string   `bson:"title"`
 	CreationDate string   `bson:"creationDate"`
@@ -49,4 +54,74 @@ type SuprisingException struct {
 
 func (e *SuprisingException) Message() string {
 	return e.Err.Error()
+}
+
+
+
+
+func NewItem(authorKey string, message string) *Item {
+	subject, message := extractSubject(message)
+	return &Item{
+		AuthorKey:    authorKey,
+		CreationDate: time.Now().Format(time.RFC3339),
+		Subject:      subject,
+		Message:      message,
+	}
+}
+
+func extractSubject(message string) (string, string) {
+	message = strings.TrimSpace(message)
+	return tryExtractExplicitSubject(message,
+				extractImplicitSubject)
+}
+
+/*
+	Subject is explicitly marked in message: Subject is enclosed in "<"...">" and
+	is located right at the beginning of the message.
+	The subject is extracted and then removed from the message.
+ */
+func tryExtractExplicitSubject(message string,
+	 						   onNotFound func (string) (string,string)) (string,string) {
+	if len(message) > 0 && message[0:1] == "<" {
+		endOfSubjectIndex := strings.Index(message,">")
+		if endOfSubjectIndex < 0 {endOfSubjectIndex = len(message)}
+		subject := message[1:endOfSubjectIndex]
+
+		startOfMessageBody := endOfSubjectIndex+1
+		if startOfMessageBody < len(message) {
+			message = message[startOfMessageBody:len(message)]
+		} else {
+			message = ""
+		}
+
+		return subject, message
+	} else {
+		return onNotFound(message)
+	}
+}
+
+/*
+	Subject is taken from first couple of chars in message.
+	If subject is less than the message "..." is appended.
+	The message itself remains unchanged.
+ */
+func extractImplicitSubject(message string) (string,string) {
+	const MAX_LEN_SUBJECT int = 15
+	var subject string
+
+	lenSubject := MAX_LEN_SUBJECT
+	if lenSubject > len(message) {
+		lenSubject = len(message)
+	}
+	subject = message[0:lenSubject]
+	if lenSubject < len(message) {
+		subject += "..."
+	}
+
+	subject = strings.Replace(subject, "\n", " ", -1)
+
+	if subject == "" {
+		subject = "?"
+	}
+	return subject, message
 }
