4e73ca8e9271caa321af790d771d7d59a7c9a9e0
Florian Boehmak <florian@fnbk.cc>
2017-10-24 08:47:00 +0200

    remove different versions of main()

--- src/main.go
+++ src/main.go
@@ -13,61 +13,6 @@ import (
 var VersionNumber string = ""
 
 func main() {
-	// choose one
-	//main1()
-	//main2()
-	main3()
-}
-
-// - SLA
-func main1() {
-	cliParams := NewCLIParams(VersionNumber)
-
-	// MongoDB
-	mongoDBAdapter := providers.MongoDBAdapter{ConnectionString: cliParams.MongoDBURL}
-	mongoDBAdapter.Start()
-	defer mongoDBAdapter.Stop()
-
-	// Email
-	emailNotifications := providers.EmailNotifications{
-		Domain:            cliParams.Domain,
-		NoReplyEmail:      cliParams.SMTPNoReplyEmail,
-		SMTPServerAddress: cliParams.SMTPServerAddress,
-		Username:          cliParams.SMTPUsername,
-		Password:          cliParams.SMTPPassword,
-	}
-
-	// Request Handlers
-	interactions := interactions.NewInteractions(&mongoDBAdapter, &emailNotifications)
-	requestHandlers := []portals.RequestHandler{
-		&request_handlers.AddItem{Interactions: interactions},
-		&request_handlers.CreateBox{Interactions: interactions},
-		&request_handlers.GetBox{Interactions: interactions},
-		&request_handlers.Version{VersionNumber: VersionNumber},
-		&request_handlers.StaticContent{},
-	}
-
-	// HTTP-Portal
-	httpPortal := portals.HTTPPortal{RequestHandlers: requestHandlers}
-	httpPortal.Run(cliParams.Port)
-}
-
-// - zu viele Zeilen
-func main2() {
-	cliParams := NewCLIParams(VersionNumber)
-
-	mongoDBAdapter := NewMongoDBAdapter(cliParams)
-	emailNotifications := NewEmailNotifications(cliParams)
-
-	interactions := NewInteractions(mongoDBAdapter, emailNotifications)
-
-	requestHandlers := NewRequestHandlers(interactions)
-	httpPortal := NewHTTPPortal(requestHandlers)
-
-	httpPortal.Run(cliParams.Port)
-}
-
-func main3() {
 	// config
 	cliParams := NewCLIParams(VersionNumber)
 
@@ -75,7 +20,7 @@ func main3() {
 	mongoDBAdapter, emailNotifications := NewProviders(cliParams)
 	defer mongoDBAdapter.Stop()
 	interactions := NewInteractions(mongoDBAdapter, emailNotifications)
-	httpPortal := NewHTTPPortal2(interactions)
+	httpPortal := NewHTTPPortal(interactions)
 
 	// run
 	httpPortal.Run(cliParams.Port)
@@ -95,7 +40,7 @@ func NewProviders(cliParams *CLIParams) (*providers.MongoDBAdapter, *providers.E
 	return &mongoDBAdapter, emailNotifications
 }
 
-func NewHTTPPortal2(interactions *interactions.Interactions) *portals.HTTPPortal {
+func NewHTTPPortal(interactions *interactions.Interactions) *portals.HTTPPortal {
 	requestHandlers := []portals.RequestHandler{
 		&request_handlers.AddItem{Interactions: interactions},
 		&request_handlers.CreateBox{Interactions: interactions},
@@ -106,37 +51,6 @@ func NewHTTPPortal2(interactions *interactions.Interactions) *portals.HTTPPortal
 	return &portals.HTTPPortal{RequestHandlers: requestHandlers}
 }
 
-func NewMongoDBAdapter(cliParams *CLIParams) *providers.MongoDBAdapter {
-	mongoDBAdapter := providers.MongoDBAdapter{ConnectionString: cliParams.MongoDBURL}
-	mongoDBAdapter.Start()
-	defer mongoDBAdapter.Stop()
-	return &mongoDBAdapter
-}
-
-func NewEmailNotifications(cliParams *CLIParams) *providers.EmailNotifications {
-	return &providers.EmailNotifications{
-		Domain:            cliParams.Domain,
-		NoReplyEmail:      cliParams.SMTPNoReplyEmail,
-		SMTPServerAddress: cliParams.SMTPServerAddress,
-		Username:          cliParams.SMTPUsername,
-		Password:          cliParams.SMTPPassword,
-	}
-}
-
 func NewInteractions(mongoDBAdapter *providers.MongoDBAdapter, emailNotifications *providers.EmailNotifications) *interactions.Interactions {
 	return interactions.NewInteractions(mongoDBAdapter, emailNotifications)
-}
-
-func NewRequestHandlers(interactions *interactions.Interactions) []portals.RequestHandler {
-	return []portals.RequestHandler{
-		&request_handlers.AddItem{Interactions: interactions},
-		&request_handlers.CreateBox{Interactions: interactions},
-		&request_handlers.GetBox{Interactions: interactions},
-		&request_handlers.Version{VersionNumber: VersionNumber},
-		&request_handlers.StaticContent{},
-	}
-}
-
-func NewHTTPPortal(requestHandlers []portals.RequestHandler) *portals.HTTPPortal {
-	return &portals.HTTPPortal{RequestHandlers: requestHandlers}
 }
