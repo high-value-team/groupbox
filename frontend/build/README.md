16e83c01c585936776b5dd1c709fdcc2e5f64dfc
Florian Boehmak <florian@fnbk.cc>
2018-04-05 16:14:11 +0200

    improve build system

--- frontend/build/README.md
+++ frontend/build/README.md
@@ -20,9 +20,16 @@ yarn install
 ## Run Tasks
 
 ```
-run setup  // initial setup of environment files
-run start  // start frontend on local machine
-run build  // build frontend into bin directory
-run deploy // deploy to dropstack
+run setup                           - Create environment files, e.g. env.production. Please edit files with useful values!
+run start                           - Run frontend start scripts using env.development
+run start:development               - Run frontend start scripts using env.development
+run start:production                - Run frontend start scripts using env.production
+run deploy                          - Create deploy folder and deploy to Dropstack
+run deploy:clean                    - Remove all "deploy" folders
+run build                           - Run frontend build scripts using env.production
+run build:production                - Run frontend build scripts using env.production
+run build:development               - Run frontend build scripts using env.development
+run build:clean                     - Remove all "bin" folders
 ```
 
+Execute `run` to list all available tasks
--- frontend/build/runfile.js
+++ frontend/build/runfile.js
@@ -8,61 +8,105 @@ const fs = require('fs');
 //
 
 function setup() {
-    ['env.development', 'env.production', 'env.dropstack'].forEach((filename) => {
+    const envFiles = ['env.development', 'env.production', 'env.dropstack'];
+    let allEnvFilesExist = true;
+    envFiles.forEach((filename) => {
+        if (!fs.existsSync(filename)) {
+            allEnvFilesExist = false;
+        }
+    });
+
+    if (allEnvFilesExist) {
+        console.log("All environment files already exist.")
+    }
+
+    envFiles.forEach((filename) => {
         if (!fs.existsSync(filename)) {
             run(`cp examples/${filename} .`);
+            console.log(`Please edit this file!`)
         }
     });
 }
-help(start, 'Setup environment files; copy ./example/* to .; Please edit copied files with useful values based.');
+help(setup, 'Create environment files, e.g. env.production. Please edit files with useful values!');
 
-function start () {
-    const envObj = loadEnvironment('development.env');
+function start_development () {
+    const envFile = 'env.development';
+    const envObj = loadEnvironment(envFile);
+    console.log(`using ${envFile}`);
     run(`cd ../src && yarn start`, {env: envObj});
 }
-help(start, 'Run frontend start scripts');
+help(start_development, 'Run frontend start scripts using env.development');
+
+function start_production () {
+    const envFile = 'env.production';
+    const envObj = loadEnvironment(envFile);
+    console.log(`using ${envFile}`);
+    run(`cd ../src && yarn start`, {env: envObj});
+}
+help(start_production, 'Run frontend start scripts using env.production');
 
 function build_production () {
-    console.log('using env.production');
-    _build('env.production');
+    const envFile = 'env.production';
+    console.log(`using ${envFile}`);
+    _build(envFile);
 }
-help(build_production, 'Run frontend build scripts: production');
+help(build_production, 'Run frontend build scripts using env.production');
 
 function build_development () {
-    console.log('using env.development');
-    _build('env.development');
+    const envFile = 'env.development';
+    console.log(`using ${envFile}`);
+    _build(envFile);
 }
-help(build_development, 'Run frontend build scripts: development');
+help(build_development, 'Run frontend build scripts using env.development');
 
 function _build (envPath) {
     const envObj = loadEnvironment(envPath);
     run(`cd ../src && yarn build`, {env: envObj});
-    run(`rm -rf ../bin`);
-    run(`cp -r ../src/build ../bin`);
+    run(`cp -r ../src/build bin.${timestamp()}`);
 }
+
+function build_clean() {
+    fs.readdirSync('.').forEach(file => {
+        if (fs.statSync(file).isDirectory() && file.match(/^bin\.[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}$/)) {
+            const removeDirectory = `rm -rf ${file}`;
+            run(removeDirectory);
+        }
+    });
+
+}
+help(build_clean, 'Remove all "bin" folders');
 
 function deploy () {
-    // create deploy folder: ../deploy
-    const productionEnv = loadEnvironment('production.env');
-    run(`cd ../src && yarn build`, {env: productionEnv});
-    run(`rm -rf ../deploy`);
-    run(`cp -r ../src/build ../deploy`);
+    const binPath = findNewestBinFolder();
+    if (binPath === undefined) {
+        console.log('No bin-folder found. Please execute a "build" job first!');
+        return
+    }
 
-    // parse dropstack environment variables
-    const dropstackEnv = loadEnvironment('dropstack.env');
+    const deployPath = `deploy.${timestamp()}`;
+    const createDeployFolder = `cp -r ${binPath} ${deployPath}`;
+    run(createDeployFolder);
 
-    // build .dropstack.json file
+    // create and write .dropstack.json in deploy folder
+    const dropstackEnv = loadEnvironment('env.dropstack');
     var file = fs.readFileSync('template.dropstack.json', 'utf8')
     var parsedFile = interpolate(file, dropstackEnv);
-    fs.writeFileSync('../deploy/.dropstack.json', parsedFile);
-    // run(`cat ../deploy/.dropstack.json`);
+    fs.writeFileSync(`${deployPath}/.dropstack.json`, parsedFile);
 
-    // deploy to dropstack
-    const cmd = `cd ../deploy && dropstack deploy --compress --verbose --alias ${dropstackEnv.DROPSTACK_ALIAS}.cloud.dropstack.run --token ${dropstackEnv.DROPSTACK_TOKEN}`;
-    run(cmd);
+    const deployToDropstack = `cd ${deployPath} && dropstack deploy --compress --verbose --alias ${dropstackEnv.DROPSTACK_ALIAS}.cloud.dropstack.run --token ${dropstackEnv.DROPSTACK_TOKEN}`;
+    run(deployToDropstack);
 }
-help(deploy, 'Run frontend deployment scripts');
+help(deploy, 'Create deploy folder and deploy to Dropstack');
 
+function deploy_clean() {
+    fs.readdirSync('.').forEach(file => {
+        if (fs.statSync(file).isDirectory() && file.match(/^deploy\.[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}$/)) {
+            const removeDirectory = `rm -rf ${file}`;
+            run(removeDirectory);
+        }
+    })
+}
+help(deploy_clean, 'Remove all "deploy" folders');
 
 //
 // helper
@@ -98,20 +142,39 @@ function loadEnvironment(envPath) {
     return env.parsed;
 }
 
+function findNewestBinFolder() {
+    let binFolders = [];
+    fs.readdirSync('.').forEach(file => {
+        if (fs.statSync(file).isDirectory() && file.match(/^bin\.[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}$/)) {
+            binFolders.push(file);
+        }
+    });
+    let sorted = binFolders.sort();
+    return sorted[sorted.length-1];
+}
+
+function timestamp() {
+    const pad = (n) => String("00" + n).slice(-2);
+    const date = new Date();
+    return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDay())}-${pad(date.getHours())}${pad(date.getMinutes())}${pad(date.getSeconds())}`
+}
+
 //
 // exports
 //
 
 module.exports = {
     setup,
-    start,
-    deploy,
 
-    // examples:
-    // run build
-    // run build:production
-    // run build:development
+    start: start_development,
+    'start:development': start_development,
+    'start:production': start_production,
+
+    deploy,
+    'deploy:clean': deploy_clean,
+
     'build': build_production,
     'build:production': build_production,
     'build:development': build_development,
+    'build:clean': build_clean,
 }
