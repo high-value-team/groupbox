ffb5d92a060c574dd6d7de5f1bd69b7077deca9e
Florian Boehmak <florian@fnbk.cc>
2018-05-10 19:11:55 +0200

    add imprint

--- development/frontend/src/src/Routes.js
+++ development/frontend/src/src/Routes.js
@@ -3,12 +3,14 @@ import { Router, Route, IndexRoute } from 'react-router';
 import MainContainer from './containers/MainContainer';
 import BoxContainer from './containers/BoxContainer';
 import HomeContainer from './containers/HomeContainer';
+import ImprintContainer from './containers/ImprintContainer';
 
 export default function getRoutes (history) {
   return (
     <Router history={history}>
       <Route path="/" component={MainContainer}>
         <Route path="/:boxkey" component={BoxContainer} />
+        <Route path="/imprint" component={ImprintContainer} />
         <IndexRoute component={HomeContainer}/>
       </Route>
     </Router>
